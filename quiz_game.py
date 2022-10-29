ba1da9c904df4c6f5213bcb616a8bc781da9f29b
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2022-10-29 20:24:55 +0530

    Create quiz_game.py

---
+++ quiz_game.py
@@ -0,0 +1,32 @@
+print("Welcome to this game")
+
+playing = input("Do you want to play? ")
+
+if playing.lower != "yes":
+  quit()
+  
+print ("  Okay! Let's Play  ")
+
+answer =input("What is Bitcoin based on? ")
+if answer == "Blockchain":
+  print('Thats Correct!')
+else:
+  print('Opps! You got it wrong ')
+
+answer =input("What is Solana?  ... Blockchain or brother of Polygon")
+if answer == "Blockchain":
+  print('Thats Correct!')
+else:
+  print('Opps! You got it wrong ')
+  
+answer =input("Complete this - Bit____ ")
+if answer == "coin":
+  print('Thats Correct!')
+else:
+  print('Opps! You got it wrong ')
+  
+answer =input("Is this game good? ")
+if answer == "Yes":
+  print('Thats Correct!')
+else:
+  print('Opps! You got it wrong ')
